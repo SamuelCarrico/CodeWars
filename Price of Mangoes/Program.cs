d2808e1708f6b6b529e81710c20a27fc9c82a2ed
Samuel <81042672+SasoleCarran@users.noreply.github.com>
2021-04-06 21:56:48 +0200

    Price of mangoes

---
+++ Price of Mangoes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Price_of_Mangoes
+{
+    /*There's a "3 for 2" (or "2+1" if you like) offer on mangoes. 
+     * For a given quantity and price (per mango), calculate the total cost of the mangoes.
+     */ 
+
+    class Program
+    {
+        int CalculatePrice(int quantity,int price)
+        {
+           int nbreGratuit = quantity - quantity / 3;
+            return nbreGratuit * price;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+            Console.WriteLine(test.CalculatePrice(3,3).ToString());                    
+        }
+    }
+}
