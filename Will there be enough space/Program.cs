b167e2b9667d79240240a7c5ad1ee88adc664317
Samuel <81042672+SasoleCarran@users.noreply.github.com>
2021-04-11 12:36:48 +0200

    Will there be enough space?

---
+++ Will there be enough space/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Math;
+
+namespace Will_there_be_enough_space
+{
+    class Program
+    {
+  public static int Enough(int cap, int on, int wait)
+  {
+      if((wait+on) > cap)
+        {
+        return Abs(cap-(wait+on));
+        }
+      else return 0;
+    }
+
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
