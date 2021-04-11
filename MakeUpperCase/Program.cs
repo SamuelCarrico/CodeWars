96fe7d849d97b5c3089c68e9afeedd90072ae617
Samuel <81042672+SasoleCarran@users.noreply.github.com>
2021-04-11 10:17:20 +0200

    UpperCase_CodeWars

---
+++ MakeUpperCase/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MakeUpperCase
+{
+    class Program
+    {
+
+  public static string MakeUpperCase(string str)
+        {
+          str = Console.ReadLine();
+          return str.ToUpper();
+            
+        }
+          static void Main(string[] args)
+        {
+            Console.WriteLine(MakeUpperCase("coucou"));
+        }
+    }
+}
---
+++ Regex validate PIN code/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Regex_validate_PIN_code
+{
+    class Program
+    {
+        /*ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+        If the function is passed a valid PIN string, return true, else return false.
+        */
+       
+       
+
+        public static bool ValidatePin(string pin)
+        {
+            if (pin.Length != 4 || pin.Length != 6)
+                return false;
+                      
+            else 
+            { 
+                pin.ToCharArray();
+                foreach (int digits in pin)
+                {
+                    Console.WriteLine(digits);
+
+                }
+                
+            }
+            return true;
+        }
+            static void Main(string[] args)
+        {
+            
+            if(ValidatePin("12222234") == true)
+            {             
+                Console.WriteLine("Vrai");
+                typeof(int);
+            }
+            else Console.WriteLine("Faux");
+        }
+    }
+}
