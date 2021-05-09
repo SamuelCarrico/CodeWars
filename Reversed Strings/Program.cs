0fb434408bce6398a45c607f2ca66168913c943f
Samuel <carricosamuel@gmail.com>
2021-05-09 13:26:51 +0200

    Reversed Strings

---
+++ Reversed Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using static System.Console;
+
+namespace Reversed_Strings
+{
+    class Program
+    {
+          public static string Solution(string str) 
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);    
+        }
+
+
+        static void Main(string[] args)
+        {
+           Solution("Try");
+        }
+    }
+}
