e76f69a3aa8cae21230e87eeb9de58b61b9da9c6
Samuel <carricosamuel@gmail.com>
2021-06-08 19:14:19 +0200

    Descending Order

---
+++ Descending Order/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+	public static int DescendingOrder(int num)
+	{
+		string numString = num.ToString();
+		char[] charArray = numString.ToCharArray();
+		Array.Sort<char>(charArray);
+		Array.Reverse<char>(charArray);
+		string str = new string (charArray);
+		int strInt = Convert.ToInt32(str);
+		return strInt;
+	}
+
+	public static void Main()
+	{
+		DescendingOrder(951234);
+	}
+}
