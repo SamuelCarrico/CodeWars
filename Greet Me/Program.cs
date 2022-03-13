b63035816136e1042af7816711547efb814a483b
Samuel <carricosamuel@gmail.com>
2022-03-13 19:10:43 +0100

    Greet Me

---
+++ Greet Me/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+*/
+
+namespace Greet_Me
+{
+    class Program
+    {
+        public static string Greet(string name)
+        {
+            string last = name.Substring(1);
+            string first = name.Remove(1);
+            string firstToUpper = first.ToUpper();
+            string lastToLower = last.ToLower();
+            Console.WriteLine($"Hello {firstToUpper}{lastToLower}");
+            return first;
+        }
+
+        static void Main(string[] args)
+        {
+           Greet("baCK");
+        }
+    }
+}
